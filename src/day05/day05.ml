f6fe77b8d653590dbc34c4a54336b8efacea8ad8
Damien Ragoucy <dam@ramdam.io>
2021-03-15 21:01:11 +0100

    Add day 5 part 2

--- src/day05/day05.ml
+++ src/day05/day05.ml
@@ -57,12 +57,39 @@ let rec wip_seat_to_seat = function
     Some { row = row; column = col; id = row * 8 + col; }
   | _ -> None
 
-let result_part_1 =
+let seats =
   input
   |> List.map ~f:(fun s -> { remaining_letters = String.to_list s;
                              row_range = (0, 127);
                              column_range = (0, 7); })
   |> List.map ~f:wip_seat_to_seat
+
+(* Part 1 *)
+
+let result_part_1 =
+  seats
   |> List.map ~f:(function Some x -> Some x.id | None -> None)
   |> List.map ~f:(fun x -> Option.value_exn x)
   |> List.max_elt ~compare:Int.compare
+
+(* Part 2 *)
+
+type row = Full | Partial of seat list
+type seat_completion = (int, row, Int.comparator_witness) Map.t
+
+let result_part_2 =
+  seats
+  |> List.map ~f:(fun x -> Option.value_exn x)
+  |> List.fold
+    ~init:((Map.empty (module Int)) : seat_completion)
+    ~f:begin fun acc seat ->
+        match Map.find acc seat.row with
+        | None -> Map.set acc ~key:seat.row ~data:(Partial [seat])
+        | Some Full -> acc
+        | Some (Partial l) when List.length l = (8 - 1) -> Map.set acc ~key:seat.row ~data:Full
+        | Some (Partial l) -> Map.set acc ~key:seat.row ~data:(Partial (seat :: l))
+    end
+  |> Map.filter ~f:(function Full -> false | _ -> true)
+
+let printable_result_part_2 =
+  result_part_2 |> Map.to_alist
