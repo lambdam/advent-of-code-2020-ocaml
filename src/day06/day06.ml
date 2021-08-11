d7367d1124b983a509bb582fa6a3b3386e123124
Damien Ragoucy <dam@ramdam.io>
2021-08-11 12:40:34 +0200

    Add day 6 part 2

--- src/day06/day06.ml
+++ src/day06/day06.ml
@@ -5,7 +5,7 @@ let input =
   In_channel.read_lines "data/input.txt"
 
 type line_state = Content | Empty
-type line_acc = char list
+type line_acc = char list list
 type result = line_acc list
 
 type parse_acc = {
@@ -15,7 +15,7 @@ type parse_acc = {
   lines: string list;
 }
 
-let result_part_1 =
+let data =
   let is_empty s = s |> Caml.String.trim |> String.is_empty in
   let rec parse_lines ({line_state; line_acc; result; lines} as acc : parse_acc) =
     match (line_state, lines) with
@@ -28,15 +28,29 @@ let result_part_1 =
     (* In the middle of a block *)
     | (Content, line :: rest) ->
       parse_lines {acc with lines = rest;
-                            line_acc = (line |> String.to_list |> List.append line_acc)}
+                            line_acc = (line |> String.to_list |> (fun l -> l :: line_acc))}
     (* In case of double empty lines *)
     | (Empty, line :: rest) when is_empty line ->
       parse_lines {acc with lines = rest}
     (* Beginning of a block *)
     | (Empty, line :: rest) ->
       parse_lines {acc with lines = rest; line_state = Content;
-                            line_acc = String.to_list line}
+                            line_acc = [(String.to_list line)]}
   in
   parse_lines {line_state = Content; line_acc = []; result = []; lines = input}
-  |> List.map ~f:Set.(function x -> x |> of_list (module Char) |> length)
+  |> List.rev
+
+let result_part_1 =
+  let open List in
+  data
+  |> map ~f:(fun l -> fold l ~init:[] ~f:append)
+  |> map ~f:Set.(fun x -> x |> of_list (module Char) |> length)
   |> List.fold ~init:0 ~f:(+)
+
+let result_part_2 =
+  let open List in
+  data
+  |> map ~f:begin map ~f:(Set.of_list (module Char)) end
+  |> map ~f:Set.(reduce_exn ~f:Set.inter)
+  |> map ~f:Set.length
+  |> reduce_exn ~f:(+)
